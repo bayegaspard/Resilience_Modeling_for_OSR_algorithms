3778e9238a15fd8f98aabadd1478f8a392b0e17d
ambroggi <abroggi@umassd.edu>
2023-09-29 12:13:47 -0400

    Creating DataObject
    Creating an object to hold data to go to the frontend.

---
+++ tests/test_dataObject.py
@@ -0,0 +1,12 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from outputDataObject import outputDataUpdateObject
+
+
+def test_exists():
+    assert not outputDataUpdateObject.dummy() is None
+
+
+def test_hasUnknowns():
+    assert len(outputDataUpdateObject.dummy().unknowns) > 0
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,27 @@
 # Sample test
 # Test files must be named in the form of `*_test.py` or `test_*.py`
+import pytest
+
+
 def func(x):
     return x + 1
 
 
 def test_answer():
     assert func(3) == 4
+
+
+def test_lambdas():
+    # For refrence for how to make lambda functions and what they do.
+    assert (lambda x: x + 1)(3) == 4
+    assert (lambda x, y: x + y)(2, 5) == 7
+    assert (lambda x: func(3) == x)(4)
+
+
+def test_fail():
+    assert not func(3) == 5
+
+
+def test_except():
+    with pytest.raises(TypeError):
+        assert func({"a": 2})
